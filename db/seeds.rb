1cebb6609e9a317080312a7af6d9ce10232de70d
Juan Trejo <trejocola@gmail.com>
2020-04-22 17:35:38 -0600

    add seeds using faker config

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def set_user(email: Faker::Internet.email, passwd: 'abcdabcdab', role: :user)
+  {
+    email: email,
+    password: passwd,
+    password_confirmation: passwd,
+    role: role
+  }
+end
+
+unless User.any?
+  u = User.new(set_user(email: 'admin@example.com', role: :admin))
+  u.save
+  5.times do
+    u = User.new(set_user)
+    u.save
+  end
+end
